794409860ab7e5b84d11240f2216b03b5d28708e
MAHESH P <84432746+Maheshinformer@users.noreply.github.com>
2023-08-01 15:56:46 +0530

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,9 +1,6 @@
 <audio controls>
-<source src=
-"https://media.geeksforgeeks.org/wp-content/uploads/20190625153922/frog.mp3" type="audio/mp3">
+<source src="music.mp3" type="audio/mp3">
     </audio>
 <audio controls loop>
-<source src=
-"https://media.geeksforgeeks.org/wp-content/uploads/20190625153922/frog.mp3"
-        type="audio/mp3">
+<source src="music.mp3" type="audio/mp3">
     </audio>
